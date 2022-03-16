1085297ff4ca5535fdc889b3e169878d683ff937
ValoQuang <qtruongngoc95@gmail.com>
2022-03-16 23:16:25 +0200

    starting package

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Autocomplete } from '@react-google-maps/api';
+import {AppBar, ToolBar, Typography, InputBase, Box} from "@material-ui/core"
+import SearchIcon from '@material-ui/icons/Search';
+
+import useStyles from './styles.js';
 
 const Header = () => {
+  const classes = useStyles();
   return (
-    <div>Header</div>
+    <AppBar position="static">
+      <ToolBar class={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+        <Typography variant="h6" className={classes.title}>
+            Explore new places
+        </Typography>
+        {/* autocomplete from react-google-maps to suggest */}
+        <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+            </div>
+        </Autocomplete>
+        {/* autocomplete from react-google-maps to suggest */}
+        </Box>
+      </ToolBar>
+    </AppBar>
   )
 }
 
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+import mapStyles from '../../mapStyles';
+import useStyles from './styles.js';
 
 const Map = () => {
+  const matches = useMediaQuery('(min-width:600px)');
+  const classes = useStyles();
+
   return (
     <div>Map</div>
   )
